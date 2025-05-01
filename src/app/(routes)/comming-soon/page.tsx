61a919fc00c25f39d6b01d2a318f1b239ee40f44
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-01 20:44:20 +0900

    feat: basic setting
    basic setting about path and navigation bar, not-implemented components

---
+++ src/app/(routes)/comming-soon/page.tsx
@@ -0,0 +1,5 @@
+import NotImplemented from '@/components/NotImplemented/NotImplemented';
+
+export default function CommingSoon() {
+  return <NotImplemented />;
+}
---
+++ src/app/(routes)/more/page.tsx
@@ -0,0 +1,5 @@
+import NotImplemented from '@/components/NotImplemented/NotImplemented';
+
+export default function More() {
+  return <NotImplemented />;
+}
