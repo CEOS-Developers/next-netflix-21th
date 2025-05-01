a23b2e6c80d6a8feb7aa5c4d71ea5a7f94b319c8
only1Ksy <winterksy@gmail.com>
2025-05-01 23:54:08 +0900

    feat: landing page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import Lottie from 'lottie-react';
+
+import LogoAnimation from '@/app/assets/lottie/LogoAnimation.json';
+
 export default function Landing() {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Lottie animationData={LogoAnimation} loop={false} />
+    </div>
+  );
 }
