f36370dce17fa54a97ba16aed329fb422ee603b5
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-01 21:12:42 +0900

    test: revert to default page setting

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,3 @@
-// app/page.tsx
-export default function Page() {
-  return <div className="flex h-screen items-center justify-center text-2xl">Hello World from Root Page</div>;
+export default function Landing() {
+  return <div></div>;
 }
