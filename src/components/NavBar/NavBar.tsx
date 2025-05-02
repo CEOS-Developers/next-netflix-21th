808bd11d3e16f12ef95238d948b386d83d266e0a
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-02 15:32:48 +0900

    test: vercel deploy error
    remove Icon component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,25 @@
 'use client';
 
-// import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { navItems } from './navItems';
 
 export default function NavBar() {
-  // const pathName = usePathname();
+  const pathName = usePathname();
 
   return (
     <nav className="bg-background-01 absolute bottom-[27px] flex h-12 w-full justify-between pt-2 pr-[29px] pl-[26px]">
-      {navItems.map(({ href, label, Icon }) => {
-        return <div></div>;
+      {navItems.map(({ href, label, Icon: _ }) => {
+        const isActive = pathName.startsWith(href);
+
+        return (
+          <Link key={href} href={href} className="flex flex-col items-center">
+            <span className="flex h-6 w-6 items-center justify-center"></span>
+            <span className={`text-caption-02 ${isActive ? 'text-grayscale-02-white' : 'text-grayscale-01'}`}>
+              {label}
+            </span>
+          </Link>
+        );
       })}
     </nav>
   );
