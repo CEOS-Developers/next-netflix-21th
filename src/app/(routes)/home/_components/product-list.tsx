2212a4713daaf483575c8a92aaabebe979b04478
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-03 00:17:32 +0900

    feat: for good UX

--- src/app/(routes)/home/_components/product-list.tsx
+++ src/app/(routes)/home/_components/product-list.tsx
@@ -10,12 +10,12 @@ export default function ProductList({ title, items }: ProductListProps) {
   return (
     <section className="ml-3 flex flex-col gap-[14px]">
       <span className="text-headline-02 text-grayscale-02-white ml-1">{title}</span>
-      <div className="hide-scrollbar flex gap-[7px] overflow-x-auto">
+      <div className="hide-scrollbar flex gap-[7px] overflow-x-auto pr-3">
         {items.map((item) => {
           return (
             item.name &&
             item.image && (
-              <div key={item.id} className="flex-shrink-0">
+              <div key={item.id} className="flex-shrink-0 transition-transform duration-200 hover:scale-105">
                 <Image src={item.image} alt={item.name} width={103} height={161} className="rounded-xs" />
               </div>
             )
