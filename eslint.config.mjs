3fa1386cc8e433b9454a28467314d1ebb6df4d1e
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-02 23:59:52 +0900

    fix: off explicit-any rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:prettier/recommended'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // for TMDB item
+    },
+  },
+];
 
 export default eslintConfig;
