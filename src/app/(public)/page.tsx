113c608238a78e517a234f96b9b1b4762e252dcd
only1Ksy <winterksy@gmail.com>
2025-05-02 10:13:53 +0900

    feat: auto routing to /home after landing animation

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,13 +1,20 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import Lottie from 'lottie-react';
 
 import LogoAnimation from '@/app/assets/lottie/LogoAnimation.json';
 
 export default function Landing() {
+  const router = useRouter();
+
+  const handleComplete = () => {
+    router.push('/home');
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Lottie animationData={LogoAnimation} loop={false} />
+      <Lottie animationData={LogoAnimation} loop={false} onComplete={handleComplete} />
     </div>
   );
 }
