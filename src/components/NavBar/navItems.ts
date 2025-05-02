f516809dea3d142f0e414fd1fec8226dd425d0aa
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-02 15:47:51 +0900

    fix: vercel deploy error

--- src/components/NavBar/navItems.ts
+++ src/components/NavBar/navItems.ts
@@ -1,7 +1,7 @@
-import HomeIcon from '@public/icons/nav/home.svg';
-import SearchIcon from '@public/icons/nav/search.svg';
-import CommingSoonIcon from '@public/icons/nav/comming-soon.svg';
-import DownloadsIcon from '@public/icons/nav/downloads.svg';
+import HomeIcon from '@public/icons/nav/more.svg';
+import SearchIcon from '@public/icons/nav/more.svg';
+import CommingSoonIcon from '@public/icons/nav/more.svg';
+import DownloadsIcon from '@public/icons/nav/more.svg';
 import MoreIcon from '@public/icons/nav/more.svg';
 
 export const navItems = [
