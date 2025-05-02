13630498d0b4931f65134147d64e4a32e1f2fa77
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-02 14:34:46 +0900

    fix: tailwind order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen items-center justify-center antialiased">
-        <div className="mobile-frame relative">{children}</div>
+      <body className="min-h-screen flex items-center justify-center">
+        <div className="mobile-frame relative bg-grayscale-00-black">{children}</div>
       </body>
     </html>
   );
