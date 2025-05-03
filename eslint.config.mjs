b1fe1a8dda9eb9a224cdcc399d32bf14ae5dd891
young <azerosong@gmail.com>
2025-05-04 00:35:00 +0900

    chore: 기본적인 eslint-plugin-import 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,65 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends(
+		'next/core-web-vitals',
+		'next',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+		'plugin:prettier/recommended',
+	),
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parserOptions: {
+				project: ['./tsconfig.json'],
+			},
+		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					project: './tsconfig.json',
+				},
+			},
+			'import/parsers': {
+				'@typescript-eslint/parser': ['**/*.ts', '**/*.tsx'],
+			},
+		},
+		rules: {
+			'prettier/prettier': ['error', { endOfLine: 'auto' }],
+			'import/extensions': [
+				'error',
+				'ignorePackages',
+				{
+					js: 'never',
+					jsx: 'never',
+					ts: 'never',
+					tsx: 'never',
+				},
+			],
+			'import/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'internal', 'index', 'type', 'parent', 'sibling'],
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+					'newlines-between': 'always',
+				},
+			],
+		},
+	},
 ];
 
 export default eslintConfig;
