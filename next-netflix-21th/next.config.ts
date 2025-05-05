293bf4b1cc1f8d4d7e0efb43dc7124ac1ae5eee1
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-05-05 21:45:58 +0900

    chore: svgr/webpack 의존성 설치

--- next-netflix-21th/next.config.ts
+++ next-netflix-21th/next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["image.tmdb.org"], //외부 이미지 도메인 추가
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
