bae43807adaf90a8c1d0643bee2674ba3abc1ea9
only1Ksy <winterksy@gmail.com>
2025-05-05 21:03:04 +0900

    design: apply hover style on search-list component

--- src/app/(routes)/search/_components/search-list.tsx
+++ src/app/(routes)/search/_components/search-list.tsx
@@ -20,7 +20,10 @@ export default function SearchList({ data }: SearchListProps) {
       {data?.pages.map((page, pageIndex) => (
         <ul key={pageIndex}>
           {page.map((movie: Movie) => (
-            <li key={movie.id} className="bg-background-02 mb-1 flex w-full items-center justify-between">
+            <li
+              key={movie.id}
+              className="bg-background-02 mb-1 flex w-full items-center justify-between rounded-sm transition-transform duration-200 hover:scale-105"
+            >
               <img src={movie.image} alt={movie.name} className="h-[76px] w-[100px]" />
               <div className="ml-6 overflow-hidden">{movie.name}</div>
               {/* 클릭시 상세 페이지로 이동 (link 걸든 div에 걸든 아므튼) 
