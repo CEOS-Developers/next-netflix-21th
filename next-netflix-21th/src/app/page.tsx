6842b43044074402f3e69d467e39f8700a755ece
chaeyoungwon <epopcy@naver.com>
2025-05-06 14:07:11 +0900

    feat: 넷플릭스 랜딩페이지 구현

--- next-netflix-21th/src/app/page.tsx
+++ next-netflix-21th/src/app/page.tsx
@@ -1,4 +1,50 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import Logo from "@/public/icons/header/logo.svg";
+import Lottie from "react-lottie-player";
+import LandingLottie from "@/public/lotties/LandingLottie.json";
+
 const Landing = () => {
-  return <div>랜딩 페이지입니다</div>;
+  const router = useRouter();
+  const [started, setStarted] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const handleStart = () => {
+    setStarted(true);
+    audioRef.current?.play();
+  };
+
+  const handleAnimationComplete = () => {
+    router.push("/home");
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-black relative">
+      {!started && (
+        <div
+          className="absolute inset-0 flex justify-center items-center z-10 cursor-pointer"
+          onClick={handleStart}
+        >
+          <Logo
+            width={300}
+            height="auto"
+            className="cursor-pointer transform hover:scale-110 transition-transform duration-300 ease-in-out"
+          />
+        </div>
+      )}
+      <Lottie
+        loop={false}
+        animationData={LandingLottie}
+        play={started}
+        speed={1.25}
+        className="w-full h-full"
+        onComplete={handleAnimationComplete}
+      />
+      <audio src="/sounds/NetflixIntroSound.mp3" ref={audioRef} />
+    </div>
+  );
 };
+
 export default Landing;
