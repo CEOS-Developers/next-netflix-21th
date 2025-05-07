3916ce283d62af808075237be173f4e230ca2c68
chaeyoungwon <epopcy@naver.com>
2025-05-07 17:40:39 +0900

    fix: heroslider 스와이퍼 디자인 수정 및 slide 방향 수정

--- next-netflix-21th/src/components/home/HeroSlider.tsx
+++ next-netflix-21th/src/components/home/HeroSlider.tsx
@@ -1,72 +1,84 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IMAGE_BASE_URL } from "@/constants/tmdb";
 import { getTrendingAllDay } from "@/apis/tmdb";
 import { TrendingItem } from "@/types/tmdb";
 import Image from "next/image";
 import Top10Icon from "@/public/icons/home/Top10Icon.svg";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules"; // Import Autoplay module
+import { Swiper as SwiperClass } from "swiper/types";
 import "swiper/css";
 
-const HeroSlider= () =>{
+const HeroSlider = () => {
   const [trendingData, setTrendingData] = useState<TrendingItem[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const swiperRef = useRef<SwiperClass | null>(null);
 
   useEffect(() => {
     const fetchTrending = async () => {
       try {
-        setLoading(true);
         const response = await getTrendingAllDay();
-        setTrendingData(response.data.results);
+        setTrendingData(response.data.results.slice(0, 10));
       } catch (error) {
         console.error("트랜딩 데이터 가져오기 실패", error);
-        setError("Failed to load trending data.");
-      } finally {
-        setLoading(false);
       }
     };
     fetchTrending();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>{error}</div>;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (swiperRef.current) {
+        swiperRef.current.slideNext(); // 항상 오른쪽으로만 넘어감
+      }
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={1}
-      autoplay={{
-        delay: 3000, // 3 seconds
-        disableOnInteraction: true, // Pause on user interaction
-      }}
-      modules={[Autoplay]}
-    >
-      {trendingData.slice(0, 10).map((item, index) => {
-        const imageUrl = `${IMAGE_BASE_URL}original${item.poster_path}`;
-        const title = "title" in item ? item.title : item.name;
+    <div className="relative w-full h-[415px]">
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={1}
+        loop={trendingData.length > 2}
+        onSlideChange={swiper => setActiveIndex(swiper.realIndex)}
+        onBeforeInit={swiper => {
+          swiperRef.current = swiper;
+        }}
+        className="w-full h-full"
+      >
+        {trendingData.map((item, index) => {
+          const imageUrl = `${IMAGE_BASE_URL}original${item.poster_path}`;
+          const title = "title" in item ? item.title : item.name;
 
-        return (
-          <SwiperSlide key={item.id} role="listitem">
-            <div className="relative w-full h-[440px]">
-              <Image
-                src={imageUrl}
-                alt={title}
-                fill
-                priority={index < 3}
-                quality={75}
-              />
-            </div>
-            <div className="flex items-center gap-2 mt-2">
-              <Top10Icon className="w-16 h-16 " />
-              <span>#{index + 1} in Korea Today</span>
-            </div>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+          return (
+            <SwiperSlide key={item.id}>
+              <div className="relative w-full h-full overflow-hidden">
+                <Image
+                  src={imageUrl}
+                  alt={title}
+                  fill
+                  sizes="375px"
+                  priority={index < 3}
+                  quality={75}
+                  className="object-cover"
+                />
+                <div className="absolute bottom-0 left-0 w-full h-1/4 z-10 bg-gradient-to-t from-[#00000073] to-transparent" />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      <div className="absolute bottom-[-2px] left-1/2 transform -translate-x-1/2 z-20 flex items-center justify-center gap-[5px]">
+        <Top10Icon className="w-[15px] h-[15px]" />
+        <span className="text-white text-[13.72px] font-bold h-5">
+          #{activeIndex + 1} in Korea Today
+        </span>
+      </div>
+    </div>
   );
-}
+};
+
 export default HeroSlider;
