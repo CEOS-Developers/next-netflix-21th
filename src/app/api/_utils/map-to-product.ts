d1b26071d2c9e144089928fa57043c23b63225f0
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-08 14:18:56 +0900

    refactor: more strict operator

--- src/app/api/_utils/map-to-product.ts
+++ src/app/api/_utils/map-to-product.ts
@@ -4,7 +4,7 @@ import type { RawTMDB } from '@models/raw-tmdb';
 export function mapToProduct<T extends RawTMDB>(item: T, type?: Product['type']): Product {
   return {
     id: item.id,
-    type: item.media_type ?? type ?? null,
+    type: item.media_type || type || null,
     name: item.title || item.name || null,
     image: item.poster_path ? `https://image.tmdb.org/t/p/w500${item.poster_path}` : null,
     description: item.overview || null,
