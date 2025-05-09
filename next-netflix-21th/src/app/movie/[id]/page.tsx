a5c40960772e9653f9b1f0095b0c3f2a1d471b16
chaeyoungwon <epopcy@naver.com>
2025-05-09 20:49:09 +0900

    fix: async 오류 해결

--- next-netflix-21th/src/app/movie/[id]/page.tsx
+++ next-netflix-21th/src/app/movie/[id]/page.tsx
@@ -1,4 +1,4 @@
-const MovieDetail = ({ params }: { params: { id: string } }) => {
+const MovieDetail = async ({ params }: { params: { id: string } }) => {
   return <div>movie id: {params.id}의 상세페이지</div>;
 };
 
