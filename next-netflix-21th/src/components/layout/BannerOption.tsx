e11d293c32986016329bc02edb6746303e783e2f
lemoncurdyogurt <80269181+lemoncurdyogurt@users.noreply.github.com>
2025-05-10 00:08:20 +0900

    fix: global css hover선언 문제 해결

--- next-netflix-21th/src/components/layout/BannerOption.tsx
+++ next-netflix-21th/src/components/layout/BannerOption.tsx
@@ -4,17 +4,17 @@ import RightArrow from "@/public/icons/home/rightArrow.svg";
 const BannerOption = () => {
   return (
     <div className="flex justify-between items-center w-[375px] h-[87px] pt-[16px] pb-[16px] pr-[48px] pl-[48px]">
-      <button className="flex-col justify-center items-center hover:text-[#8C8787]">
+      <button className="flex flex-col justify-center items-center hover:text-gray-200">
         <MyListIcon className="mx-auto stroke-current" />
         <p>My List</p>
       </button>
       
-      <button className="w-[124px] h-[55px] bg-[#C4C4C4] rounded-[4px] flex justify-center items-center pt-[12px] pb-[12px] pr-[20px] pl-[20px] gap-2 hover:bg-[#8C8787]">
+      <button className="w-[124px] h-[55px] bg-gray-100 rounded-[4px] flex justify-center items-center pt-[12px] pb-[12px] pr-[20px] pl-[20px] gap-2 hover:bg-gray-200">
         <RightArrow/>
         <p className="text-[24px] font-semibold text-black">Play</p>
       </button>
 
-      <button className="flex-col justify-center items-center hover:text-[#8C8787]">
+      <button className="flex flex-col justify-center items-center hover:text-gray-200">
         <InfoIcon className="mx-auto stroke-current" />
         <p>Info</p>
       </button>
