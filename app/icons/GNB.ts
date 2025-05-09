4c9715c6f850f370a77797fba12186c5a474b0b2
DefineXX <keaikim77@gmail.com>
2025-05-10 03:49:21 +0900

    feat: 홈 화면 LNB 및 GNB UI 구현

---
+++ app/icons/GNB.ts
@@ -0,0 +1,7 @@
+export { default as NetflixHomeLogo } from '@/public/home-logo.svg';
+
+export { default as HomeIcon } from '@/public/TabBar/home.svg';
+export { default as SearchIcon } from '@/public/TabBar/search.svg';
+export { default as ReleaseIcon } from '@/public/TabBar/release.svg';
+export { default as DownloadIcon } from '@/public/TabBar/download.svg';
+export { default as MoreIcon } from '@/public/TabBar/more.svg';
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import Link from 'next/link';
+
+import { TAB_BAR_OPTIONS } from './constants/GNB';
+import { NetflixHomeLogo } from './icons/GNB';
+
 export default function Home() {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<div className="fixed top-0 left-0 w-screen flex items-center gap-5 py-6 px-4 bg-black">
+				<NetflixHomeLogo />
+				<ul className="w-full flex justify-between">
+					<li>TV Shows</li>
+					<li>Movies</li>
+					<li>My List</li>
+				</ul>
+			</div>
+			<div className="fixed bottom-0 left-0 bg-black-sub w-screen">
+				<ul className="flex items-center justify-between h-fit py-3 px-6 text-gray-200 caption">
+					{TAB_BAR_OPTIONS.map(({ label, icon: Icon, route }) => (
+						<li key={label}>
+							<Link href={route} className="w-9 h-fit flex flex-col items-center gap-0.5">
+								<Icon /> {label}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
