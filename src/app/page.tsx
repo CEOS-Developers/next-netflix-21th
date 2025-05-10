dc50c4109a2a6ff4d92edbb6af15e150c8bea96c
Yeongseo Kim <88617509+kkys00@users.noreply.github.com>
2025-05-10 16:42:53 +0900

    feat: Landing 페이지 구현 (#6)
    * chore: tailwind typography 시스템 설정
    
    * chore: svg icon 모음
    
    * feat: 기본 UI, 공통 컴포넌트 구현
    
    * feat: 공통 Play 버튼 컴포넌트 구현
    
    * feat: 홈페이지 상단 네비게이션 구현
    
    * feat: themoviewdb fetching 함수, url 작성
    
    * feat: 디자인 요소 조정
    
    * feat: 이미지 태그 url config 설정
    
    * feat: 원형, 사각형 포스터 컴포넌트 구현
    
    * feat: 횡단 스크롤 컨테이너 구현
    
    * feat: 카테고리별 영화 나열 구현
    
    * feat: 배너 옵션, 설명 컴포넌트 구현
    
    * feat: 홈페이지 구현
    
    * fix: image 태그 경고 수정
    
    * feat: 랜딩 페이지 이동 연결
    
    * feat: 랜딩 페이지 구현
    
    ---------
    
    Co-authored-by: leejuhi <anim0203@naver.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default async function Lending() {
-  return <div className="">lending</div>;
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Landing() {
+  const router = useRouter();
+
+  const handleVideoEnd = () => {
+    router.push("/home");
+  };
+
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <div>
+        <video
+          src="/netflix.webm"
+          autoPlay
+          muted
+          playsInline
+          onEnded={handleVideoEnd}
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
