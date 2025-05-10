915326ee553d210c72fac217a77b8f36e0ce948b
Lee JuHee <163210518+BeanMouse@users.noreply.github.com>
2025-05-10 18:27:00 +0900

    feat: 스타일 조정 (#10)

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { Movie } from "@/types/movie";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import Link from "next/link";
+import Play from "@/assets/play.svg";
 
 export default function DetailView() {
   const { id } = useParams();
@@ -27,15 +27,22 @@ export default function DetailView() {
   if (!movie) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-        alt={movie.title}
-        width={200}
+    <div className="flex flex-col justify-center text-white overflow-auto scrollbar-hide h-[calc(100vh-100px)] box-border">
+      <div
+        className="w-full min-h-[415px] bg-cover bg-top box-border"
+        style={{
+          backgroundImage: `linear-gradient(180deg,rgba(0, 0, 0, 0.45) 0%,rgba(0, 0, 0, 0) 50%, #000000 100%), url(https://image.tmdb.org/t/p/w500${movie.poster_path})`,
+        }}
       />
-      <p>{movie.overview}</p>
-      <Link href={`/search`}>돌아가기</Link>
+      <button
+        className="flex flex-row items-center bg-neutral-700 p-2 rounded-md justify-center mx-[36px] gap-3"
+        style={{ backgroundColor: "#C4C4C4" }}
+      >
+        <Play />
+        <p style={{ color: "black", fontWeight: "600" }}>Play</p>
+      </button>
+      <p className="m-[32px] text-[27px] font-bold">Preview</p>
+      <p className="mx-[32px]">{movie.overview}</p>
     </div>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,6 +3,9 @@
 import { Movie } from "@/types/movie";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import PlayIcon from "@/assets/playicon.svg";
+import Search from "@/assets/search.svg";
+import Close from "@/assets/Vector.svg";
 
 export default function Home() {
   const [query, setQuery] = useState("");
@@ -33,22 +36,38 @@ export default function Home() {
 
   return (
     <div>
-      <input
-        className="border w-full rounded-md p-2"
-        placeholder="Search for a show, movie, genre, e.t.c."
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <h1>Top Search</h1>
-      <ul>
+      <div className="flex w-full h-[52px] bg-neutral-700 text-white items-center p-3 px-3">
+        <Search width={28} height={28} />
+        <input
+          className="w-full h-[52px] bg-neutral-700 text-white p-2"
+          placeholder="Search for a show, movie, genre, e.t.c."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+        />
+        <Close
+          onClick={() => setQuery("")}
+          className="min-w-[15px] min-h-[15px]"
+        />
+      </div>
+      <div className="p-3 font-bold text-[26px]">Top Search</div>
+      <ul className="flex flex-col overflow-auto scrollbar-hide h-[calc(100vh-175px)] gap-[3px]">
         {results.map((movie) => (
-          <li key={movie.id} className="flex flex-row items-center">
+          <li
+            key={movie.id}
+            className="flex flex-row items-center bg-neutral-700 "
+          >
             <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
               alt={movie.title}
-              width={200}
+              width={146}
             />
-            <p>{movie.title}</p>
-            <Link href={`/movie/${movie.id}`}>보러가기</Link>
+            <div className="flex flex-row items-center justify-between w-full p-3 font-normal">
+              <p className="font-normal">{movie.title}</p>
+
+              <Link href={`/movie/${movie.id}`} className="p">
+                <PlayIcon width={28} height={28} />
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
