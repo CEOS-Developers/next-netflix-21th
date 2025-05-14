6423ed3908dd6b79f392e041397e8179e45f4ab9
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-05-14 18:28:15 +0900

    fix: nav item ordering
    design: svg size and position
    
    fix: nav items vertical order

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -9,16 +9,18 @@ export default function NavBar() {
   const pathName = usePathname();
 
   return (
-    <nav className="bg-background-01 absolute bottom-[27px] z-990 flex h-12 w-full justify-between pt-2 pr-[29px] pl-[26px]">
+    <nav className="bg-background-01 absolute bottom-[27px] z-990 flex h-12 w-full justify-between pt-2">
       {navItems.map(({ href, label, Icon }) => {
         const isActive = pathName.startsWith(href);
 
         return (
-          <Link key={href} href={href} className="flex flex-col items-center">
-            <span className="flex h-6 w-6 items-center justify-center">
+          <Link key={href} href={href} className="flex flex-1 flex-col items-center">
+            <span className="flex h-6 w-6">
               <Icon className={isActive ? 'text-grayscale-02-white' : 'text-grayscale-01'} />
             </span>
-            <span className={`text-caption-02 ${isActive ? 'text-grayscale-02-white' : 'text-grayscale-01'}`}>
+            <span
+              className={`text-caption-02 mt-[-10px] ${isActive ? 'text-grayscale-02-white' : 'text-grayscale-01'}`}
+            >
               {label}
             </span>
           </Link>
