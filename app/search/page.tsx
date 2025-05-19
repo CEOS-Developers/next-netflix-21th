33a0f03c7bc95a6f3e88e159a86545d76f281135
DefineXX <keaikim77@gmail.com>
2025-05-19 16:14:03 +0900

    fix: 검색 결과에 대한 무한 스크롤 구현 및 검색 로직 수정

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,51 +4,72 @@ import { useEffect, useRef, useState } from 'react';
 
 import { useSearchStore } from '@/stores/searchStore';
 import { getTrendingMovies, searchMovies } from '@/services/tmdb';
+import { MediaInfos } from '@/types/movie.types';
 
 import SearchBar from '@/components/features/search/SearchBar';
 import SearchCard from '@/components/features/search/SearchCard';
-import { Movie } from '@/types/movie.types';
 
 const SearchPage = () => {
-	const { query, results, setResults } = useSearchStore();
-	const [hasMore, setHasMore] = useState<boolean>(true);
+	const { query, setQuery, results, setResults } = useSearchStore();
+	const [hasMore, setHasMore] = useState<boolean>(false);
 	const [page, setPage] = useState<number>(1);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
 	const observer = useRef<IntersectionObserver | null>(null);
 	const loaderRef = useRef<HTMLDivElement | null>(null);
+	const isFirstPage = useRef(page === 1);
+
+	useEffect(() => {
+		setQuery('');
+	}, []);
+
+	useEffect(() => {
+		// 검색어 변경 시 페이지 및 결과 초기화
+		setPage(1);
+		setResults([]);
+		isFirstPage.current = true;
+	}, [query]);
 
 	useEffect(() => {
 		const fetchSearchResults = async () => {
 			try {
-				const services = query ? searchMovies(query) : getTrendingMovies(page);
+				setIsLoading(true); // API 호출 시작
+				const services = query.trim() !== '' ? searchMovies(query, page) : getTrendingMovies(page);
 				const data = await services;
-				const movieInfoList = data.results as Movie[];
+				console.log(data);
+				const movieInfoList = data.results as MediaInfos;
 
-				if (query) {
-					setResults(movieInfoList || []);
-				} else {
-					setResults((prev: Movie[]) => {
-						const newItems = (movieInfoList || []).filter((item) => !prev.some((p) => p.id === item.id));
-						return [...prev, ...newItems];
-					});
-					setHasMore(data.page < data.total_pages);
-				}
+				setResults((prev: MediaInfos) => {
+					const newItems = (movieInfoList || []).filter((item) => !prev?.some((p) => p.id === item.id));
+					return page === 1 ? newItems : [...prev, ...newItems];
+				});
+
+				// Check if there are more pages to fetch
+				setHasMore(data.page < data.total_pages);
 			} catch (error) {
-				console.error('Error fetching trending movies:', error);
+				console.error('Error fetching search page data:', error);
+			} finally {
+				setIsLoading(false); // API 호출 완료
 			}
 		};
 
 		fetchSearchResults();
-	}, [query, setResults, page]);
+	}, [query, page]);
 
 	useEffect(() => {
 		if (!loaderRef.current) return;
 
 		if (observer.current) observer.current.disconnect();
+
 		observer.current = new IntersectionObserver(
 			(entries) => {
 				if (entries[0].isIntersecting && hasMore) {
-					setPage((prev) => prev + 1);
+					if (isFirstPage.current) {
+						isFirstPage.current = false;
+						return;
+					} else {
+						setPage((prev) => prev + 1);
+					}
 				}
 			},
 			{ threshold: 1 },
@@ -57,26 +78,32 @@ const SearchPage = () => {
 	}, [query, hasMore, loaderRef]);
 
 	return (
-		<div>
+		<div className="pb-[65px]">
 			<SearchBar />
 
 			<div>
-				<h1 className="headline1 pl-4 mt-5 mb-4">{query !== '' ? 'Search Results' : 'Top Searches'}</h1>
-				{results.length === 0 ? (
-					<p>No results found.</p>
+				<h1 className="headline1 pl-4 mt-5 mb-4">{query.trim() !== '' ? 'Search Results' : 'Top Searches'}</h1>
+				{isLoading ? (
+					<p className="w-full h-100 flex items-center justify-center text-gray-300 body1">Loading...</p>
+				) : results.length === 0 ? (
+					<p className="w-full h-100 flex items-center justify-center text-gray-300 headline2-medium">
+						No results found.
+					</p>
 				) : (
 					<ul className="flex flex-col gap-1">
-						{results?.map((result, index) => (
-							<li key={result.id} className="w-full h-fit">
-								<SearchCard result={result} priority={index < 9 ? true : false} />
-							</li>
-						))}
+						{results.map((result, index) => {
+							return (
+								<li key={result.id} className="w-full h-fit">
+									<SearchCard result={result} priority={index < 9 ? true : false} />
+								</li>
+							);
+						})}
 					</ul>
 				)}
 			</div>
 
 			{/* 무한 스크롤 감지 */}
-			<div ref={loaderRef} className="h-10" />
+			<div ref={loaderRef} />
 		</div>
 	);
 };
